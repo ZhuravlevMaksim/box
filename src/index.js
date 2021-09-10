9dcdc3780c027beedbfd1455852b17afaa58d20c
Zhuravlev Maksim <maks723@yandex.ru>
2021-09-10 16:55:37 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {Canvas} from "./Canvas";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Canvas />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
